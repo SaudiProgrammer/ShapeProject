231e28b0a7a84f5b513d5a6a5b9c7bc7c97bdaf0
SaudiProgrammer <121711622+SaudiProgrammer@users.noreply.github.com>
2022-12-31 14:18:03 +0300

    Before aggregation

---
+++ Ch10_Aggregation/Test.java
@@ -0,0 +1,12 @@
+package Ch9_With_OOP;
+
+public class Test {
+	public static void main(String[] args) {
+		Circle x = new Circle(20);
+
+	    System.out.println(
+	    		"The area for the circle of radius " +
+	    		x.getRadius() + " is " + x.getArea());
+	}
+
+}

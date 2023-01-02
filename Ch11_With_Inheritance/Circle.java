57a3106488b1232b3d101eb103171a188b9bccac
SaudiProgrammer <121711622+SaudiProgrammer@users.noreply.github.com>
2023-01-02 08:06:29 +0300

    Before inheritance

---
+++ Ch11_With_Inheritance/Circle.java
@@ -0,0 +1,36 @@
+package C11_Without_Inheritance;
+
+//Aggregated Class
+public class Circle {
+	private double radius;
+	private Coordinates coordinates;
+	
+	public Circle(double r, Coordinates coordinates){
+		setRadius(r);
+		this.coordinates = coordinates;
+	}
+	
+	public Circle(double r,int x, int y){
+		this(r, new Coordinates(x,y));
+	}
+	
+	public Circle(){
+		this(1,0,0);
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public double getArea() {
+		return Math.PI * getRadius() * getRadius();
+	}
+	
+	public String getShapeLocation() {
+		return coordinates.getLocation();
+	}
+}
---
+++ Ch11_With_Inheritance/Test.java
@@ -0,0 +1,27 @@
+package C11_Without_Inheritance;
+
+public class Test {
+	public static void main(String[] args) {
+		Circle x = new Circle(20,1,1);
+
+	    System.out.println(
+	    		"The area for the circle of radius " +
+	    		x.getRadius() + " is " + x.getArea());
+	    System.out.println("The circle is located in the "+
+	    		x.getShapeLocation());
+	    
+	    System.out.println("===========================================");
+	    
+	    Rectangle y = new Rectangle(10,10,-1,-1);
+
+	    System.out.println(
+	    		"The area for the rectangle of width " +
+	    		y.getW() + " and length " + y.getL() +
+	    		" is " + y.getArea());
+	    System.out.println("The rectangle is located in the "+
+	    		y.getShapeLocation());
+	    
+	    System.out.println("===========================================");
+	    
+	}
+}
